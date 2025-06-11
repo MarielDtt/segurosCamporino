c44d07aea9a48e4abf464e5fdeeca4ad998772f5
MarielDtt <marieldruetta@gmail.com>
2025-06-11 20:35:52 -0300

    Navbar Responsive

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,24 +2,32 @@
 "use client"
 import Image from 'next/image';
 import { MenuCustomAnimation } from './MenuCustomAnimation';
+import Link from 'next/link';
 
 const Navbar = () => {
 
     return (
 
         <nav className="flex w-full bg-primary-600 h-16 px-4 py-2 justify-between items-center lg:h-24 lg:px-16 lg:py-4 ">
-         <div>
-            <MenuCustomAnimation/>
-         </div>
-            <div>
+            <div className='block lg:hidden'>
+                <MenuCustomAnimation />
+            </div>
+
+            <div className="relative w-7 h-7 lg:w-14 lg:h-14">
                 <Image
                     src="/Logo_C.png"
-                    width={28}
-                    height={28}
+                    fill
                     alt="Logo Simple Seguros Camporino"
                 />
             </div>
-
+            <div className='hidden lg:flex'>
+                <div className='flex text-Background-Lilac text-title3 gap-x-6'>
+                    <Link href="#porque-elegirnos" className='hover:text-secondary-300'> ¿Por qué elegirnos? </Link>
+                     <Link href="#planes" className='hover:text-secondary-300'> Planes y Coberturas </Link>
+                     <Link href="/preguntas-frecuentes" className='hover:text-secondary-300'>Preguntas Frecuentes </Link>
+                     <Link href="#contacto" className='hover:text-secondary-300'> Contacto </Link>
+                </div>
+            </div>
         </nav >
     )
 }
