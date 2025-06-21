9e3a348633e151437a332c0fbf0ef6ef7c601ea2
MarielDtt <marieldruetta@gmail.com>
2025-06-20 21:16:09 -0300

    Agregamos lazy en componentes form y reseña

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,15 @@ import ScrollableIcon from "@/components/scrollableIcon";
 import SplashScreen from "@/components/splashscreen";
 import Image from 'next/image';
 import { useState, useEffect } from "react";
-import { AnimatePresence  } from "motion/react"
+import { AnimatePresence } from "motion/react"
+import dynamic from 'next/dynamic'
+
 
 export default function Home() {
   const [splashActive, setSplashActive] = useState<boolean>(false);
+
+  const ComponentResenia = dynamic(() => import('../components/resenia'))
+  const ComponentForm = dynamic(() => import('../components/form'))
 
   useEffect(() => {
     const splash = setTimeout(() => {
@@ -25,15 +30,15 @@ export default function Home() {
         !splashActive ? (<SplashScreen key="splash" />) :
           (<div className="w-auto h-auto lg:w-auto space-y-6 lg:space-y-10 m-auto">
             <div className="lg:hidden flex h-auto m-auto mt-6 px-6 justify-center items-center bg-primary-600">
-                <Image
+              <Image
                 src="/General_Mobile.webp"
                 alt="Descripcion General de Seguros para Mobile"
                 width={350}
                 height={302}
               />
             </div>
-             <div className="hidden lg:flex w-full justify-center items-center bg-primary-600">
-                <Image
+            <div className="hidden lg:flex w-full justify-center items-center bg-primary-600">
+              <Image
                 src="/GeneralCompleto.png"
                 alt="Descripcion General de Seguros para Mobile"
                 width={1200}
@@ -50,10 +55,10 @@ export default function Home() {
               <Elegirnos />
             </div>
             <div className="w-[390px] h-auto lg:w-[880px] flex justify-center items-center bg-Background-Default m-auto">
-              <Resenia />
+              <ComponentResenia />
             </div>
             <div className="w-[390px] h-auto lg:w-[556px] flex justify-center items-center bg-Background-Default m-auto">
-              <Form />
+              <ComponentForm/>
             </div>
             <a
               href="https://wa.me/5491161076870?text=Hola%2C%20quiero%20cotizar%20un%20seguro"
